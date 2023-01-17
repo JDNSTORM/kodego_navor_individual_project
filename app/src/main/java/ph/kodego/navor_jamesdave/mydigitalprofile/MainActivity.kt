e7eabc9abb107b60100592071ab3c40a25f99295
James Dave Navor <esteban.dave999@gmail.com>
2023-01-17 15:40:37 +0800

    Initial Setup

--- app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/MainActivity.kt
+++ app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/MainActivity.kt
@@ -2,10 +2,14 @@ package ph.kodego.navor_jamesdave.mydigitalprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.kodego.navor_jamesdave.mydigitalprofile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
