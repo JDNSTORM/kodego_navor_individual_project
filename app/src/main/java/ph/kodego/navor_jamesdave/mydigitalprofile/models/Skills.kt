62041aa2dc359439ba4fcce23702a7b39a0c3bbb
James Dave Navor <esteban.dave999@gmail.com>
2023-03-26 18:18:46 +0800

    Checkpoint!!!: Restructured Skills Model Structure Return before this commit in case of errors

--- app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/models/Skills.kt
+++ app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/models/Skills.kt
@@ -1,21 +1,58 @@
 package ph.kodego.navor_jamesdave.mydigitalprofile.models
 
-data class SkillMainCategory(
-    var id: Int = 0,
-    val profileID: Long,
-    var categoryMain: String = "",
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+open class SkillMainCategory(
+    var categoryMainID: String = "",
+    val profileID: String = "",
+    var categoryMain: String = ""
+): Parcelable{
     val subCategories: ArrayList<SkillSubCategory> = ArrayList()
-)
 
-data class SkillSubCategory(
-    var id: Int = 0,
-    val categoryMainID: Int,
+//    companion object : Parceler<SkillMainCategory> {
+//        override fun SkillMainCategory.write(parcel: Parcel, flags: Int) {
+//            // Custom write implementation
+//        }
+//
+//        override fun create(parcel: Parcel): SkillMainCategory {
+//            // Custom read implementation
+//        }
+//    }
+    constructor( //TODO: Remove after trials
+        categoryMainID: String = "",
+        profileID: String = "",
+        categoryMain: String = "",
+        subCategories: ArrayList<SkillSubCategory> = ArrayList()
+    ): this(categoryMainID, profileID, categoryMain){
+        this.subCategories.addAll(subCategories)
+    }
+}
+
+@Parcelize
+open class SkillSubCategory(
+    var categorySubID: String = "",
     var categorySub: String = "",
+): SkillMainCategory(), Parcelable{
     val skills: ArrayList<Skill> = ArrayList()
-)
 
+    constructor( //TODO: Remove after trials
+        categorySubID: String = "",
+        categorySub: String = "",
+        skills: ArrayList<Skill> = ArrayList()
+    ): this(categorySubID, categorySub){
+        this.skills.addAll(skills)
+    }
+    constructor(categoryMainID: String): this(){
+        this.categoryMainID = categoryMainID
+    }
+}
+
+@Parcelize
 data class Skill(
-    var id: Int = 0,
-    val categorySubID: Int,
-    var skill: String
-)
+    var skillID: String = "",
+    var skill: String = ""
+): SkillSubCategory(), Parcelable
