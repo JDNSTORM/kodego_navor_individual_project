2671b6702121c3e7d5a26541c7eb76750d77d0c9
James Dave Navor <esteban.dave999@gmail.com>
2023-03-30 18:17:33 +0800

    Extended FirebaseAccountDAO functions

--- app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/firebase/FirebaseAccountDAO.kt
+++ app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/firebase/FirebaseAccountDAO.kt
@@ -3,18 +3,18 @@ package ph.kodego.navor_jamesdave.mydigitalprofile.firebase
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import kotlinx.coroutines.tasks.await
 import ph.kodego.navor_jamesdave.mydigitalprofile.models.Account
 import ph.kodego.navor_jamesdave.mydigitalprofile.models.ContactInformation
 import ph.kodego.navor_jamesdave.mydigitalprofile.models.EmailAddress
-import ph.kodego.navor_jamesdave.mydigitalprofile.utils.Constants
 
 interface FirebaseAccountDAO {
     suspend fun addAccount(account: Account): Boolean
     suspend fun registerAccount(firstName: String, lastName: String, email: String, password: String): Boolean
     suspend fun getAccount(uID: String): Account?
+    suspend fun updateAccount(fields: HashMap<String, Any>): Boolean
+    suspend fun deleteAccount()
 }
 
 open class FirebaseAccountDAOImpl(context: Context): FirebaseUserDAOImpl(context), FirebaseAccountDAO{
@@ -63,7 +63,7 @@ open class FirebaseAccountDAOImpl(context: Context): FirebaseUserDAOImpl(context
             .document(uID)
             .get()
         task.await()
-        Log.d("Get Account", task.result.toString())
+        Log.d("Get Account", task.result.toString(), task.exception)
         return if (task.isSuccessful && task.result.data != null){
             Log.i("Account Document Retrieved", task.result.toString())
             val account = task.result.toObject(Account::class.java)!!
@@ -73,6 +73,25 @@ open class FirebaseAccountDAOImpl(context: Context): FirebaseUserDAOImpl(context
             Log.e("Account Error", task.exception!!.message.toString())
             null
         }
+    }
+
+    override suspend fun updateAccount(fields: HashMap<String, Any>): Boolean {
+        val task = fireStore
+            .collection(collection)
+            .document(getCurrentUserID())
+            .update(fields)
+        task.await()
+        Log.d("Update Account", task.result.toString(), task.exception)
+        return task.isSuccessful
+    }
+
+    override suspend fun deleteAccount() {
+        val task = fireStore
+            .collection(collection)
+            .document(getCurrentUserID())
+            .delete()
+        task.await()
+        Log.d("Delete Account", task.result.toString(), task.exception)
     }
 
 }
