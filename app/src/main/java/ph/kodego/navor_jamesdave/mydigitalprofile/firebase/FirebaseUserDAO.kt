085e2cd4ca88c554de509a3f92c7149b4ec68c5e
James Dave Navor <esteban.dave999@gmail.com>
2023-04-03 10:18:58 +0800

    Debugged SignIn Exception Prepared Updating Password

--- app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/firebase/FirebaseUserDAO.kt
+++ app/src/main/java/ph/kodego/navor_jamesdave/mydigitalprofile/firebase/FirebaseUserDAO.kt
@@ -2,7 +2,11 @@ package ph.kodego.navor_jamesdave.mydigitalprofile.firebase
 
 import android.content.Context
 import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
@@ -12,6 +16,7 @@ interface FirebaseUserDAO {
     suspend fun signInUser(email: String, password: String): Boolean
     fun signOutUser()
     fun getCurrentUserID(): String
+    suspend fun updateUserPassword(password: String): Boolean
 }
 
 open class FirebaseUserDAOImpl(internal val context: Context): FirebaseUserDAO{
@@ -30,14 +35,15 @@ open class FirebaseUserDAOImpl(internal val context: Context): FirebaseUserDAO{
     }
 
     override suspend fun signInUser(email: String, password: String): Boolean { //TODO: AsyncTask
-        val task = auth.signInWithEmailAndPassword(email, password)
-        task.await()
-        return if (task.isSuccessful){
-            true
-        }else{
-            Toast.makeText(context, task.exception?.message.toString(), Toast.LENGTH_SHORT).show()
-            false
+        val task: Task<AuthResult>
+        try {
+            task = auth.signInWithEmailAndPassword(email, password)
+            task.await()
+        }catch (e: FirebaseAuthException){
+            Toast.makeText(context, e.message.toString(), Toast.LENGTH_SHORT).show()
+            return false
         }
+        return task.isSuccessful
     }
 
     override fun signOutUser() {
@@ -45,8 +51,21 @@ open class FirebaseUserDAOImpl(internal val context: Context): FirebaseUserDAO{
     }
 
     override fun getCurrentUserID(): String {
-        val currentUser = auth.currentUser
-        return currentUser?.uid ?: ""
+        val user = auth.currentUser
+        return user?.uid ?: ""
+    }
+
+    override suspend fun updateUserPassword(password: String): Boolean {
+        val user = auth.currentUser!!
+        val task: Task<Void>
+        try {
+            task = user.updatePassword(password)
+            task.await()
+        }catch (e: FirebaseException){
+            Toast.makeText(context, e.message.toString(), Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return task.isSuccessful
     }
 
 }
